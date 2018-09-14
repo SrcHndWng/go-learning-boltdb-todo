1e3155f9f4a59754f90014b8d443d89ad9eaa25c
UnderSourceCode <undersourcecode@hotmail.co.jp>
2018-09-14 16:16:04 +0900

    boltdbへの登録、参照を実装。

--- actions/create.go
+++ actions/create.go
@@ -1,13 +1,20 @@
 package actions
 
 import (
-	"fmt"
-
+	"github.com/SrcHndWng/go-learning-boltdb-todo/dataAccess"
 	"github.com/urfave/cli"
 )
 
 // Create registers your toto.
 func Create(c *cli.Context) error {
-	fmt.Println("create!! todo = " + c.Args().First())
+	todo, err := dataAccess.NewTodo()
+	if err != nil {
+		return err
+	}
+
+	err = todo.Create(c.Args().First())
+	if err != nil {
+		return err
+	}
 	return nil
 }
--- actions/list.go
+++ actions/list.go
@@ -3,11 +3,21 @@ package actions
 import (
 	"fmt"
 
+	"github.com/SrcHndWng/go-learning-boltdb-todo/dataAccess"
 	"github.com/urfave/cli"
 )
 
 // List prints your todos
 func List(c *cli.Context) error {
-	fmt.Println("list!!")
+	// fmt.Println("list!!")
+	todo, err := dataAccess.NewTodo()
+	if err != nil {
+		return err
+	}
+	todos, err := todo.List()
+	if err != nil {
+		return err
+	}
+	fmt.Println(todos)
 	return nil
 }
